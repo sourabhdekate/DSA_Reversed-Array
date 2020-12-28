1350582fc7998da092d3cb34e14e5f93006d6082
Sourabh Dekate <75211569+sourabhdekate@users.noreply.github.com>
2020-12-29 01:23:13 +0530

    Add files via upload

---
+++ src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int arr[] = {1, 2, 3, 4, 5, 6};
+	printArray(arr, 6);
+	reverseArray(arr, 0, 5);
+	printArray(arr,6);
+    }
+     static void reverseArray(int arr[], int start, int end){
+        int temp;
+
+        while (start<end){
+            temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+     }
+     static void printArray(int arr[], int size){
+        for (int i =0; i<size; i++){
+            System.out.println(arr[i] + " ");
+            System.out.println();
+        }
+     }
+}
